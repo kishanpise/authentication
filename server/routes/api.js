20e4092c2137c0b1293d52fc83bf55ca7d850b1a
kishan <kishanpise2@gmail.com>
2021-04-10 18:34:05 +0530

    third commit

--- server/routes/api.js
+++ server/routes/api.js
@@ -1,6 +1,17 @@
 const express = require('express')
 const router = express.Router()
 
+const mongoose = require('mongoose')
+const db = "mongodb://userkishan:pwkishan@eventsdb.cozyd.mongodb.net/eventdb"
+
+mongoose.connect(db, err => {
+    if(err){
+        console.error('Error!'+err)
+    } else{
+        console.log("Connected to mongodb")
+    }
+})
+
 router.get('/', (req, res)=>{
     res.send('From API Route')
 })
